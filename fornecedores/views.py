c7d36f4b71ad67af9a83cc9248bd5f1ca1ae8f57
Thierry <thierryflowers@gmail.com>
2024-10-16 14:19:11 -0300

    home

--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from .forms import FornecedorModelForm
 from .models import Fornecedor
@@ -18,5 +18,16 @@ class FornecedoresView(ListView):
 class FornecedorAddView(CreateView):
     model = Fornecedor
     form_class = FornecedorModelForm
+    template_name = 'fornecedor_form.html'
+    success_url = reverse_lazy('fornecedores')
+
+class FornecedorUpdateView(UpdateView):
+    model = Fornecedor
+    form_class = FornecedorModelForm
+    template_name = 'fornecedor_form.html'
+    success_url = reverse_lazy('fornecedores')
+
+class FornecedorDeleteView(DeleteView):
+    model = Fornecedor
     template_name = 'fornecedor_form.html'
     success_url = reverse_lazy('fornecedores')
