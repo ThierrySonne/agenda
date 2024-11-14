81f9ed324745ef9e092823f9d24e016b9c61b49d
Thierry <thierryflowers@gmail.com>
2024-11-14 16:12:47 -0300

    home

--- agenda/settings.py
+++ agenda/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS, EMAIL_BACKEND
+from django.conf.global_settings import STATICFILES_DIRS, EMAIL_BACKEND, LOGIN_REDIRECT_URL, LOGIN_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +142,8 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thierryflowers@gmail.com'
 EMAIL_HOST_PASSWORD = 'nltfryoqrceeyhvs'
 DEFAULT_FROM_EMAIL = 'Lavacar'
+
+
+LOGIN_REDIRECT_URL = 'index'
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'login'
